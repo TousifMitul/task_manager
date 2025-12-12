819bfbb663da800731ceb41fd62f687c184b0f31
TousifMitul <tousif5robbani@gmail.com>
2025-12-12 22:35:49 +0600

    update profile

--- lib/ui/screens/update_profile.dart
+++ lib/ui/screens/update_profile.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/sign_in.dart';
 import 'package:task_manager/ui/widgets/background.dart';
 
+import '../widgets/tm_app_bar.dart';
+
 class UpdateProfile extends StatefulWidget {
   const UpdateProfile({super.key});
 
@@ -16,6 +18,7 @@ class _UpdateProfileState extends State<UpdateProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: TMAppBar(),
       body: Background(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
@@ -23,9 +26,9 @@ class _UpdateProfileState extends State<UpdateProfile> {
             crossAxisAlignment: CrossAxisAlignment.start,
             spacing: 8,
             children: [
-              SizedBox(height: 60),
+              SizedBox(height: 36),
               Text(
-                'Join With Us',
+                'Update Profile',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
               ),
               SizedBox(height: 8),
@@ -37,30 +40,6 @@ class _UpdateProfileState extends State<UpdateProfile> {
               FilledButton(
                 onPressed: _onTapSignUp,
                 child: Icon(Icons.arrow_circle_right_outlined),
-              ),
-              SizedBox(height: 24),
-              Center(
-                child: Column(
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        text: 'Already have an account?  ',
-                        children: [
-                          TextSpan(
-                              text: 'Sign In',
-                              style: TextStyle(color: Colors.blue),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = _onTapSignIn
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
               ),
             ],
           ),
