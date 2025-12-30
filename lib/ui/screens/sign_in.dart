712e0982e5c7805ede71ad01de973237358fbe5d
TousifMitul <tousif5robbani@gmail.com>
2025-12-30 22:32:46 +0600

    isLoggedIn

--- lib/ui/screens/sign_in.dart
+++ lib/ui/screens/sign_in.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/navholder.dart';
 import 'package:task_manager/ui/screens/sign_up.dart';
 import 'package:task_manager/ui/widgets/background.dart';
-
 import '../../data/models/user_model.dart';
 import '../../data/service/network_caller.dart';
 import '../../data/utils/urls.dart';
@@ -26,91 +25,93 @@ class _SignInState extends State<SignIn> {
   final TextEditingController _passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  bool _signInInProgress = false;
+  bool _signInProgress = false;
 
 
   @override
   Widget build(BuildContext context) {
     return Background(
-      child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 8,
-            children: [
-              SizedBox(height: 60),
-              Text(
-                'Get Started With',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              SizedBox(height: 8),
-              TextFormField(
-                  controller: _emailController,
-                  validator: (String? value) {
-                    if (value?.trim().isEmpty ?? true) {
-                      return 'Please enter your email';
-                    }
-                    if (EmailValidator.validate(value!) == false) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(hintText: 'Email')),
-              TextFormField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  validator: (String? value) {
-                    if (value?.isEmpty ?? true) {
-                      return 'Please enter your password';
-                    }
-                    if (value!.length < 6) {
-                      return 'Password must be at least 6 characters';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(hintText: 'Password')),
-              Visibility(
-                visible: _signInInProgress==false,
-                replacement: Center(
-                  child: CircularProgressIndicator(),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8,
+              children: [
+                SizedBox(height: 60),
+                Text(
+                  'Get Started With',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                child: FilledButton(
-                  onPressed: _onTapSignIn,
-                  child: Icon(Icons.arrow_circle_right_outlined),
+                SizedBox(height: 8),
+                TextFormField(
+                    controller: _emailController,
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Please enter your email';
+                      }
+                      if (EmailValidator.validate(value!) == false) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: 'Email')),
+                TextFormField(
+                    obscureText: false,
+                    controller: _passwordController,
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Please enter your password';
+                      }
+                      if (value!.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: 'Password')),
+                Visibility(
+                  visible: _signInProgress==false,
+                  replacement: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  child: FilledButton(
+                    onPressed: _onTapSignIn,
+                    child: Icon(Icons.arrow_circle_right_outlined),
+                  ),
                 ),
-              ),
-              SizedBox(height: 24),
-              Center(
-                child: Column(
-                  children: [
-                    TextButton(
-                      onPressed: _onTapForgotPassword,
-                      child: Text('Forgot Password'),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        text: 'Don\'t have an account?  ',
-                        children: [
-                          TextSpan(
-                            text: 'Sign Up',
-                            style: TextStyle(color: Colors.blue),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = _onTapSignUp,
-                          ),
-                        ],
+                SizedBox(height: 24),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: _onTapForgotPassword,
+                        child: Text('Forgot Password'),
                       ),
-                    ),
-                  ],
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          text: 'Don\'t have an account?  ',
+                          children: [
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(color: Colors.blue),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = _onTapSignUp,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -119,12 +120,14 @@ class _SignInState extends State<SignIn> {
 
   void _onTapSignIn() {
     if (_formKey.currentState!.validate()) {
-
+      _signIn();
     }
   }
   Future <void> _signIn() async {
-    _signInInProgress = true;
-    setState(() {});
+    _signInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
     Map<String, dynamic> requestBody = {
       "email": _emailController.text.trim(),
       "password": _passwordController.text,
@@ -133,12 +136,18 @@ class _SignInState extends State<SignIn> {
       Urls.login,
       body: requestBody,);
     if (response.isSuccess) {
-      UserModel userModel = UserModel.fromJson(response.body['body']);
+      UserModel userModel = UserModel.fromJson(response.body['data']);
       String accessToken = response.body['token'];
       await AuthController.saveUserData(accessToken, userModel);
-      Navigator.pushReplacementNamed(context, NavHolder.name);
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, NavHolder.name);
+      }
     } else {
-      showSnackBarMsg(context, response.errorMessage);
+      _signInProgress = false;
+      if (mounted) {
+        setState(() {});
+        showSnackBarMsg(context, response.errorMessage ?? 'Login Failed! Please try again');
+      }
     }
 
   }
