117b93ee49dff2c7bcfb786bed4d7154d2907864
TousifMitul <tousif5robbani@gmail.com>
2026-01-02 23:24:34 +0600

    isLoggedIn

--- lib/ui/screens/sign_in.dart
+++ lib/ui/screens/sign_in.dart
@@ -128,7 +128,6 @@ class _SignInState extends State<SignIn> {
     if (mounted) {
       setState(() {});
     }
-
     Map<String, dynamic> requestBody = {
       "email": _emailController.text.trim(),
       "password": _passwordController.text,
