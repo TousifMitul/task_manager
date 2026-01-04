e91b32c59a3b3f95397cb65a0c7013ca36ae40ae
TousifMitul <tousif5robbani@gmail.com>
2026-01-04 23:26:59 +0600

    add new

--- lib/ui/screens/add_new.dart
+++ lib/ui/screens/add_new.dart
@@ -13,6 +13,10 @@ class AddNew extends StatefulWidget {
 }
 
 class _AddNewState extends State<AddNew> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,30 +25,42 @@ class _AddNewState extends State<AddNew> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(24),
-            child: Column(
-              spacing: 8,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 36,),
-                Text('Add New Task', style: TextTheme.of(context).titleLarge),
-                SizedBox(height: 8,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Title',
+            child: Form(
+              key: _formKey,
+              child: Column(
+                spacing: 8,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 36,),
+                  Text('Add New Task', style: TextTheme.of(context).titleLarge),
+                  SizedBox(height: 8,),
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Title'),
+                      validator: (String? value) {
+                        if (value
+                            ?.trim()
+                            .isEmpty ?? true) {
+                          return 'Please enter a title';
+                        }
+                        return null;
+                      }
+                    ),
                   ),
-                ),
-                TextFormField(
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    hintText: 'Description',
+                  TextFormField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      hintText: 'Description',
+                    ),
                   ),
-                ),
-                SizedBox(height: 8,),
-                FilledButton(
-                  onPressed: () {},
-                  child: Icon(Icons.arrow_circle_right_outlined),
-                ),
-              ],
+                  SizedBox(height: 8,),
+                  FilledButton(
+                    onPressed: () {},
+                    child: Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
