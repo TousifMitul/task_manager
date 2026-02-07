cdfa5d485c607a8ea146772521977ba72e6370e4
TousifMitul <tousif5robbani@gmail.com>
2026-02-07 22:51:49 +0600

    add new

--- lib/ui/screens/add_new.dart
+++ lib/ui/screens/add_new.dart
@@ -87,7 +87,6 @@ class _AddNewState extends State<AddNew> {
   Future<void> _addNewTask() async {
     _addNewTaskInProgress = true;
     setState(() {});
-
     Map<String, dynamic> requestBody = {
       "title": _titleController.text.trim(),
       "description": _descriptionController.text.trim(),
