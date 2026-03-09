e5ccd619e7f8ac741f1cd084cbfea1fbc4a2cdf3
TousifMitul <tousif5robbani@gmail.com>
2026-03-09 19:28:18 +0600

    add new

--- lib/ui/screens/add_new.dart
+++ lib/ui/screens/add_new.dart
@@ -17,7 +17,6 @@ class AddNew extends StatefulWidget {
 class _AddNewState extends State<AddNew> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _addNewTaskInProgress = false;
 
