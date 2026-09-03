5fc5b3eb54aff2ff7f635975512d56e8e7b5efce
TousifMitul <tousif5robbani@gmail.com>
2026-09-03 16:21:13 +0600

    add new

--- lib/ui/screens/add_new.dart
+++ lib/ui/screens/add_new.dart
@@ -38,7 +38,6 @@ class _AddNewState extends State<AddNew> {
                   Text('Add New Task', style: TextTheme.of(context).titleLarge),
                   SizedBox(height: 8),
                   TextFormField(
-                    
                     controller: _titleController,
                     decoration: InputDecoration(hintText: 'Title'),
                     validator: (String? value) {
